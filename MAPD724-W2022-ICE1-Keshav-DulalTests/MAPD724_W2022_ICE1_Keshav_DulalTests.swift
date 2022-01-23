91f9215fa3f7f61a1b8f1174fa5de5f206262c46
keshavdulal <Keshav.dulal@gmail.com>
2022-01-23 22:31:21 +0545

    Initial Commit

---
+++ MAPD724-W2022-ICE1-Keshav-DulalTests/MAPD724_W2022_ICE1_Keshav_DulalTests.swift
@@ -0,0 +1,33 @@
+//
+//  MAPD724_W2022_ICE1_Keshav_DulalTests.swift
+//  MAPD724-W2022-ICE1-Keshav-DulalTests
+//
+//  Created by Keshav Dulal on 23/01/2022.
+//
+
+import XCTest
+@testable import MAPD724_W2022_ICE1_Keshav_Dulal
+
+class MAPD724_W2022_ICE1_Keshav_DulalTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
